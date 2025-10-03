8d1bffc45e3b5838a28d5cc5bcd6ec7a160e3339
Yashawini <muraliyasha11@gmail.com>
2025-10-03 21:32:10 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    inventory = Column(Integer)
+    description = Column(String, nullable=True)  # optional now; you can add later per assignment
